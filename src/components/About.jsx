870e8437c289d2afb21440d8d7e3070cb2520c08
officialalihaider <aliseeyam@gmail.com>
2024-09-17 11:52:58 +0500

    name changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,83 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div className='container mt-[5rem] mb-10 m-auto max-md:w-[95vw]'>
-      <h1 className='font-bold text-2xl py-2 max-md:text-xl max-md:mx-2'>About NexBookStore</h1>
-      <p className='text-lg text-slate-600 p-5 text-justify max-md:text-base max-sm:text-sm max-sm:p-2 max-sm:px-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet iste sapiente aspernatur, doloremque quam tempora consequuntur quo neque qui repudiandae perferendis dolor, accusantium pariatur distinctio eligendi dicta veniam expedita accusamus similique voluptatem assumenda molestias excepturi. Odit placeat quod fugiat blanditiis repudiandae consequatur. Hic veritatis accusamus magnam neque aliquid libero ullam voluptas. Quidem maiores deserunt, excepturi accusantium hic perspiciatis tenetur vero. Fugit deserunt cum quas quaerat magnam sequi nobis, non distinctio obcaecati blanditiis qui porro culpa deleniti, sapiente maiores.</p>
-      <p className='text-lg text-slate-600 p-5 text-justify max-md:text-base max-sm:text-sm max-sm:p-2 max-sm:px-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet iste sapiente aspernatur, doloremque quam tempora consequuntur quo neque qui repudiandae perferendis dolor, accusantium pariatur distinctio eligendi dicta veniam expedita accusamus similique voluptatem assumenda molestias excepturi. Odit placeat quod fugiat blanditiis repudiandae consequatur. Hic veritatis accusamus magnam neque aliquid libero ullam voluptas. Quidem maiores deserunt, excepturi accusantium hic perspiciatis tenetur vero. Fugit deserunt cum quas quaerat magnam sequi nobis, non distinctio obcaecati blanditiis qui porro culpa deleniti, sapiente maiores.</p>
-      <p className='text-lg text-slate-600 p-5 text-justify max-md:text-base max-sm:text-sm max-sm:p-2 max-sm:px-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet iste sapiente aspernatur, doloremque quam tempora consequuntur quo neque qui repudiandae perferendis dolor, accusantium pariatur distinctio eligendi dicta veniam expedita accusamus similique voluptatem assumenda molestias excepturi. Odit placeat quod fugiat blanditiis repudiandae consequatur. Hic veritatis accusamus magnam neque aliquid libero ullam voluptas. Quidem maiores deserunt, excepturi accusantium hic perspiciatis tenetur vero. Fugit deserunt cum quas quaerat magnam sequi nobis, non distinctio obcaecati blanditiis qui porro culpa deleniti, sapiente maiores.</p>
-      <h2 className='font-bold text-xl py-2 max-md:text-lg max-md:mx-2'>Why NexBookStore ?</h2>
-      <ul className='p-2 pl-10'>
-        <li className='list-disc text-lg p-2 text-justify max-md:text-base max-sm:text-sm'><p><span className='font-bold'>Lorem, ipsum dolor : </span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi minus molestiae nobis laboriosam ipsa voluptatum adipisci soluta, nisi ipsum aliquam, accusantium odio non.</p></li>
-        <li className='list-disc text-lg p-2 text-justify max-md:text-base max-sm:text-sm'><p><span className='font-bold'>Lorem, ipsum dolor : </span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi minus molestiae nobis laboriosam ipsa voluptatum adipisci soluta, nisi ipsum aliquam, accusantium odio non.</p></li>
-        <li className='list-disc text-lg p-2 text-justify max-md:text-base max-sm:text-sm'><p><span className='font-bold'>Lorem, ipsum dolor : </span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi minus molestiae nobis laboriosam ipsa voluptatum adipisci soluta, nisi ipsum aliquam, accusantium odio non.</p></li>
-        <li className='list-disc text-lg p-2 text-justify max-md:text-base max-sm:text-sm'><p><span className='font-bold'>Lorem, ipsum dolor : </span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi minus molestiae nobis laboriosam ipsa voluptatum adipisci soluta, nisi ipsum aliquam, accusantium odio non.</p></li>
+    <div className="container mt-[5rem] mb-10 m-auto max-md:w-[95vw]">
+      <h1 className="font-bold text-2xl py-2 max-md:text-xl max-md:mx-2">
+        About NextBookStore
+      </h1>
+      <p className="text-lg text-slate-600 p-5 text-justify max-md:text-base max-sm:text-sm max-sm:p-2 max-sm:px-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet iste
+        sapiente aspernatur, doloremque quam tempora consequuntur quo neque qui
+        repudiandae perferendis dolor, accusantium pariatur distinctio eligendi
+        dicta veniam expedita accusamus similique voluptatem assumenda molestias
+        excepturi. Odit placeat quod fugiat blanditiis repudiandae consequatur.
+        Hic veritatis accusamus magnam neque aliquid libero ullam voluptas.
+        Quidem maiores deserunt, excepturi accusantium hic perspiciatis tenetur
+        vero. Fugit deserunt cum quas quaerat magnam sequi nobis, non distinctio
+        obcaecati blanditiis qui porro culpa deleniti, sapiente maiores.
+      </p>
+      <p className="text-lg text-slate-600 p-5 text-justify max-md:text-base max-sm:text-sm max-sm:p-2 max-sm:px-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet iste
+        sapiente aspernatur, doloremque quam tempora consequuntur quo neque qui
+        repudiandae perferendis dolor, accusantium pariatur distinctio eligendi
+        dicta veniam expedita accusamus similique voluptatem assumenda molestias
+        excepturi. Odit placeat quod fugiat blanditiis repudiandae consequatur.
+        Hic veritatis accusamus magnam neque aliquid libero ullam voluptas.
+        Quidem maiores deserunt, excepturi accusantium hic perspiciatis tenetur
+        vero. Fugit deserunt cum quas quaerat magnam sequi nobis, non distinctio
+        obcaecati blanditiis qui porro culpa deleniti, sapiente maiores.
+      </p>
+      <p className="text-lg text-slate-600 p-5 text-justify max-md:text-base max-sm:text-sm max-sm:p-2 max-sm:px-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet iste
+        sapiente aspernatur, doloremque quam tempora consequuntur quo neque qui
+        repudiandae perferendis dolor, accusantium pariatur distinctio eligendi
+        dicta veniam expedita accusamus similique voluptatem assumenda molestias
+        excepturi. Odit placeat quod fugiat blanditiis repudiandae consequatur.
+        Hic veritatis accusamus magnam neque aliquid libero ullam voluptas.
+        Quidem maiores deserunt, excepturi accusantium hic perspiciatis tenetur
+        vero. Fugit deserunt cum quas quaerat magnam sequi nobis, non distinctio
+        obcaecati blanditiis qui porro culpa deleniti, sapiente maiores.
+      </p>
+      <h2 className="font-bold text-xl py-2 max-md:text-lg max-md:mx-2">
+        Why NextBookStore ?
+      </h2>
+      <ul className="p-2 pl-10">
+        <li className="list-disc text-lg p-2 text-justify max-md:text-base max-sm:text-sm">
+          <p>
+            <span className="font-bold">Lorem, ipsum dolor : </span>Lorem ipsum,
+            dolor sit amet consectetur adipisicing elit. Sequi minus molestiae
+            nobis laboriosam ipsa voluptatum adipisci soluta, nisi ipsum
+            aliquam, accusantium odio non.
+          </p>
+        </li>
+        <li className="list-disc text-lg p-2 text-justify max-md:text-base max-sm:text-sm">
+          <p>
+            <span className="font-bold">Lorem, ipsum dolor : </span>Lorem ipsum,
+            dolor sit amet consectetur adipisicing elit. Sequi minus molestiae
+            nobis laboriosam ipsa voluptatum adipisci soluta, nisi ipsum
+            aliquam, accusantium odio non.
+          </p>
+        </li>
+        <li className="list-disc text-lg p-2 text-justify max-md:text-base max-sm:text-sm">
+          <p>
+            <span className="font-bold">Lorem, ipsum dolor : </span>Lorem ipsum,
+            dolor sit amet consectetur adipisicing elit. Sequi minus molestiae
+            nobis laboriosam ipsa voluptatum adipisci soluta, nisi ipsum
+            aliquam, accusantium odio non.
+          </p>
+        </li>
+        <li className="list-disc text-lg p-2 text-justify max-md:text-base max-sm:text-sm">
+          <p>
+            <span className="font-bold">Lorem, ipsum dolor : </span>Lorem ipsum,
+            dolor sit amet consectetur adipisicing elit. Sequi minus molestiae
+            nobis laboriosam ipsa voluptatum adipisci soluta, nisi ipsum
+            aliquam, accusantium odio non.
+          </p>
+        </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
